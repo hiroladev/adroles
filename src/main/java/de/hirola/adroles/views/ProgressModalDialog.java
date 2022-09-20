743477481121d661a79b4997b51ba10f12235d61
hiroladev <development@hirola.de>
2022-09-20 07:55:49 +0200

    Initial commit

--- src/main/java/de/hirola/adroles/views/ProgressModalDialog.java
+++ src/main/java/de/hirola/adroles/views/ProgressModalDialog.java
@@ -10,44 +10,42 @@ package de.hirola.adroles.views;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.dialog.DialogVariant;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.progressbar.ProgressBar;
 import de.hirola.adroles.Global;
 
 public final class ProgressModalDialog {
+    private final Dialog dialog;
+    private final ProgressBar progressBar;
+    private final Div progressBarLabel, progressBarSubLabel;
 
-    private final Dialog dialog = new Dialog();
-
-    public ProgressModalDialog(String titleKey, String messageKey, String subMessageKey) {
-        buildProgressDialog(UI.getCurrent().getTranslation(titleKey),
-                UI.getCurrent().getTranslation(messageKey),
-                UI.getCurrent().getTranslation(subMessageKey));
+    public ProgressModalDialog(){
+        dialog = new Dialog();
+        progressBar = new ProgressBar();
+        progressBarLabel = new Div();
+        progressBarSubLabel = new Div();
+        buildProgressDialog();
     }
 
-    public void open() {
-        if (!dialog.isOpened()) {
-            dialog.open();
-        }
+    public void open(String titleKey, String messageKey, String subMessageKey) {
+        dialog.setHeaderTitle(UI.getCurrent().getTranslation(titleKey));
+        progressBarLabel.setText(UI.getCurrent().getTranslation(messageKey));
+        progressBarSubLabel.setText(UI.getCurrent().getTranslation(subMessageKey));
+        dialog.open();
     }
 
     public void close() {
         dialog.close();
     }
 
-    private void buildProgressDialog(String title, String message, String subMessage) {
-        ProgressBar progressBar = new ProgressBar();
+    private void buildProgressDialog() {
         progressBar.setIndeterminate(true);
-        Div progressBarLabel = new Div();
-        progressBarLabel.setText(message);
-        Div progressBarSubLabel = new Div();
         progressBarSubLabel.getStyle().set("font-size", "var(--lumo-font-size-xs)");
-        progressBarSubLabel.setText(subMessage);
-
-        dialog.setHeaderTitle(title);
         dialog.setWidth(Global.Component.DEFAULT_DIALOG_WIDTH);
         dialog.setModal(true);
         dialog.setResizable(true);
         dialog.add(progressBarLabel, progressBar, progressBarSubLabel);
     }
-
 }
